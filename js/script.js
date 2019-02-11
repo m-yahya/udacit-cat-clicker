cb3cbaa9a5e2d22146bb9661f664fb4fe2b7cc9e
Muhammad Yahya <yahya_gis@hotmail.com>
2019-02-11 22:26:00 +0100

    cat name displayed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const kittens = ['kitten1', 'kitten2', 'kitten3', 'kitten4', 'kitten5'];
 
 let main = document.querySelector('.main');
+let name = document.querySelector('.cat-name');
 for (kitten of kittens) {
   // create a div to hold kitten names
   let elem = document.createElement('div');
@@ -12,21 +13,26 @@ for (kitten of kittens) {
       let catImg = document.createElement("img");
       // add kittens according to name
       main.removeChild(main.firstChild);
-      if (elem.innerHTML === 'kitten1') {
+      if (elem.innerHTML === kittens[0]) {
         catImg.setAttribute("src", "images/kitten1.jpg");
         main.appendChild(catImg);
-      }else if (elem.innerHTML === 'kitten2') {
+        name.textContent = kittens[0];
+      }else if (elem.innerHTML === kittens[1]) {
         catImg.setAttribute("src", "images/kitten2.jpg");
         main.appendChild(catImg);
-      }else if (elem.innerHTML === 'kitten3') {
+        name.textContent = kittens[1];
+      }else if (elem.innerHTML === kittens[2]) {
         catImg.setAttribute("src", "images/kitten3.jpg");
         main.appendChild(catImg);
-      }else if (elem.innerHTML === 'kitten4') {
+        name.textContent = kittens[2];
+      }else if (elem.innerHTML === kittens[3]) {
         catImg.setAttribute("src", "images/kitten4.jpg");
         main.appendChild(catImg);
+        name.textContent = kittens[3];
       }else {
         catImg.setAttribute("src", "images/kitten5.jpg");
         main.appendChild(catImg);
+        name.textContent = kittens[4];
       }
 
     }
